628e3e9a826b42341762202909fab8cca6501095
Arvin Shen <ashen3292@gmail.com>
2022-05-12 20:04:23 -0700

    Set up day 25 milestone projects 1-3

---
+++ day25_milestone_projects01-03/RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Arvin Shen on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
