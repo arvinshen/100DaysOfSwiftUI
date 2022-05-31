7e2e09d4486bc8fea7f5e9e84e0f1205a5738271
Arvin Shen <ashen3292@gmail.com>
2022-05-30 19:45:49 -0700

    Add day 38 project 7 challenge

--- day36-38_project07/iExpense/iExpense/ContentView.swift
+++ day36-38_project07/iExpense/iExpense/ContentView.swift
@@ -14,20 +14,41 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List {
-                ForEach(expenses.items) { item in
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(item.name)
-                                .font(.headline)
-                            Text(item.type)
+                Section(header: Text("Personal")) {
+                    ForEach(expenses.personalItems) { item in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                Text(item.type)
+                            }
+                            
+                            Spacer()
+                            
+                            Text(item.amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                                .foregroundColor(self.changeColor(for: item.amount))
                         }
-                        
-                        Spacer()
-                        
-                        Text(item.amount, format: .currency(code: "USD"))
                     }
+                    .onDelete(perform: removePersonalItems)
                 }
-                .onDelete(perform: removeItems)
+                
+                Section(header: Text("Business")) {
+                    ForEach(expenses.businessItems) { item in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                Text(item.type)
+                            }
+                            
+                            Spacer()
+                            
+                            Text(item.amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                                .foregroundColor(self.changeColor(for: item.amount))
+                        }
+                    }
+                    .onDelete(perform: removeBusinessItems)
+                }
             }
             .navigationTitle("iExpense")
             .toolbar {
@@ -43,8 +64,40 @@ struct ContentView: View {
         }
     }
     
-    func removeItems(at offsets: IndexSet) {
-        expenses.items.remove(atOffsets: offsets)
+    func removePersonalItems(at offsets: IndexSet) {
+        // look at each item we're trying to delete
+        for offset in offsets {
+            // look in the personalItems array and get specific item we're trying to delete. Find corresponding match in expenses.items array.
+            if let index = expenses.items.firstIndex(where: { $0.id == expenses.personalItems[offset].id }) {
+                // delete item from expenses.items array at matched index
+                expenses.items.remove(at: index)
+            } else if let index = expenses.items.firstIndex(where: { $0.id == expenses.businessItems[offset].id }) {
+                expenses.items.remove(at: index)
+            }
+        }
+    }
+    
+    func removeBusinessItems(at offsets: IndexSet) {
+        // look at each item we're trying to delete
+        for offset in offsets {
+            // look in the businessItems array and get specific item we're trying to delete. Find corresponding match in expenses.items array.
+            if let index = expenses.items.firstIndex(where: { $0.id == expenses.businessItems[offset].id }) {
+                expenses.items.remove(at: index)
+            }
+        }
+    }
+    
+    func changeColor(for amount: Double) -> Color {
+        switch amount {
+        case -Double.infinity..<10.0:
+            return .green
+        case 10.0..<100.0:
+            return .orange
+        case 100.0..<Double.infinity:
+            return .red
+        default:
+            return .black
+        }
     }
 }
 
