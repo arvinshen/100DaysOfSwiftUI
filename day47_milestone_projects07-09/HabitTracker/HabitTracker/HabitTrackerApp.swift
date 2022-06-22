915281dd46cdc540bb28da4f9b59a377ee0a6450
Arvin Shen <ashen3292@gmail.com>
2022-06-21 22:02:36 -0700

    Set up day 47 milestone projects 7-9

---
+++ day47_milestone_projects07-09/HabitTracker/HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Arvin Shen on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
